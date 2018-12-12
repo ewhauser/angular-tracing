f4e30b303e4aea106b6323ac68a2d449ba8f240f
Eric Hauser <ewhauser@gmail.com>
2018-12-12 10:52:01 -0800

    add license

--- karma.conf.js
+++ karma.conf.js
@@ -28,7 +28,7 @@ module.exports = () => {
     colors: true,
     logLevel: constants.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ['ChromeHeadless'],
     singleRun: true
   };
 };
