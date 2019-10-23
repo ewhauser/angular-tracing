cc34a903d2c5ab6d7e165c677ccdbb89ae7974f3
Maurizio <Maurizio.CESCON@ext.ec.europa.eu>
2019-10-23 11:02:54 +0200

    fix: addition of test function to show ptoblematic refresh #43

--- apps/heroes-villains/src/app/app.component.ts
+++ apps/heroes-villains/src/app/app.component.ts
@@ -5,4 +5,9 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {}
+export class AppComponent {
+  get test(): string {
+    console.log(`Date.now: ${Date.now().toString()}`);
+    return 'test';
+  }
+}
